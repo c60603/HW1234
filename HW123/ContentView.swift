d7c527412dfae322a8cb2cd17292b5036c747b90
c60603 <c60603@gmail.com>
2020-10-06 17:13:30 +0800

    Initial Commit

---
+++ HW123/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW123
+//
+//  Created by User08 on 2020/10/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW123/HW123App.swift
@@ -0,0 +1,17 @@
+//
+//  HW123App.swift
+//  HW123
+//
+//  Created by User08 on 2020/10/6.
+//
+
+import SwiftUI
+
+@main
+struct HW123App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
